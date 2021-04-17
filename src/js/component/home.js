07620a2d61b247acfa6faa40036889a99258558b
Hittokirizero <v_hbk@hotmail.com>
2021-04-17 02:34:56 +0000

    Frame ToDo List

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love! yeaaaa
-			</p>
+			<ul className="list-group">
+				<li className="list-group-item active">
+					<h1>Todo List</h1>
+				</li>
+				<li className="list-group-item">
+					<div class="input-group mb-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Recipient's username"
+							aria-label="Recipient's username"
+							aria-describedby="basic-addon2"
+						/>
+						<div className="input-group-append">
+							<button
+								className="btn btn-outline-secondary"
+								type="button">
+								Button
+							</button>
+						</div>
+					</div>
+				</li>
+				<li className="list-group-item">Morbi leo risus</li>
+			</ul>
 		</div>
 	);
 }
