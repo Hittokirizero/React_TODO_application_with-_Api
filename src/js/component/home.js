b1779d470a53fa6dd5f0b0469f6eb7fbf553315c
Hittokirizero <v_hbk@hotmail.com>
2021-04-25 18:24:48 +0000

    Pendiente pruebas

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,29 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
 	let [task, setTask] = useState("");
 	const [taskLists, setTaskLists] = useState([]);
+	useEffect(() => {
+		fetching();
+	}, []);
 
 	const update = e => {
 		setTask(e.target.value);
 	};
 
 	const add_Task = () => {
-		if (task !== "") {
-			const taskDetails = {
-				id: Math.floor(Math.random() * 1000),
-				value: task
-			};
+		let API_todo = [...taskLists, { label: task, done: false }];
 
-			setTaskLists([...taskLists, taskDetails]);
-			console.log(taskLists);
-		}
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(API_todo);
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ZeroCR", {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		})
+			.then(response => response.json())
+			.then(data => fetching())
+			.catch(error => console.log("error", error));
 	};
 
-	const delete_Task = (e, id) => {
+	const fetching = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ZeroCR", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				if (!resp.ok) {
+					throw Error(resp.statusText);
+				}
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				console.log(data); //this will print on the console the exact object received from the server
+				setTaskLists(data);
+			})
+			.catch(error => {
+				//error handling
+				console.log("ha ocurrido un error", error);
+			});
+	};
+	fetching();
+
+	const delete_Task = e => {
 		e.preventDefault();
-		setTaskLists(taskLists.filter(t => t.id !== id));
+
+		var raw = "";
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ZeroCR", {
+			method: "DELETE",
+			body: raw,
+			redirect: "follow"
+		})
+			.then(response => response.json())
+			.then(data => setTaskLists([data]))
+			.catch(error => console.log("error", error));
 	};
 
 	const count = () => {
@@ -64,12 +107,12 @@ export function Home() {
 					{taskLists !== [] ? (
 						<div>
 							{taskLists.map(t => (
-								<li key={t.value} className="list-group-item ">
-									{t.value + " - "}
+								<li key={t.data} className="list-group-item ">
+									{t.label + " - "}
 									<button
 										className="btn btn-outline-secondary float-right-center"
 										type="button"
-										onClick={e => delete_Task(e, t.id)}>
+										onClick={e => delete_Task(e)}>
 										<i className="fas fa-times"></i>
 									</button>
 								</li>
