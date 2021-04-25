0fb607c0a7d12864db825b16ce6fccffb2b067d3
Hittokirizero <v_hbk@hotmail.com>
2021-04-25 17:41:14 +0000

    ToDoList

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,56 +1,88 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	let [task, setTask] = useState("");
+	const [taskLists, setTaskLists] = useState([]);
+
+	const update = e => {
+		setTask(e.target.value);
+	};
+
+	const add_Task = () => {
+		if (task !== "") {
+			const taskDetails = {
+				id: Math.floor(Math.random() * 1000),
+				value: task
+			};
+
+			setTaskLists([...taskLists, taskDetails]);
+			console.log(taskLists);
+		}
+	};
+
+	const delete_Task = (e, id) => {
+		e.preventDefault();
+		setTaskLists(taskLists.filter(t => t.id !== id));
+	};
+
+	const count = () => {
+		if (taskLists.length == 0) {
+			return "0";
+		} else {
+			return <str>{taskLists.length}</str>;
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<ul className="list-group">
-				<li className="list-group-item active">
-					<h1>Todo List</h1>
-				</li>
+		<div className="d-flex justify-content-center">
+			<div className="text-center mt-5">
+				<ul className="list-group">
+					<li className="list-group-item active">
+						<h1>Todo List</h1>
+					</li>
 
-				<li className="list-group-item">
-					<div className="input-group mb-3">
-						<input
-							type="text"
-							className="form-control"
-							placeholder="Add Task Name"
-							aria-label="Add Task Name"
-							aria-describedby="basic-addon2"
-						/>
-						<div className="input-group-append">
-							<button
-								className="btn btn-outline-secondary"
-								type="button">
-								Button
-							</button>
+					<li className="list-group-item">
+						<div className="input-group mb-3">
+							<input
+								onChange={e => update(e)}
+								className="form-control"
+								placeholder="Add Task"
+								aria-label="Add Task"
+								aria-describedby="basic-addon2"
+							/>
+							<div className="input-group-append">
+								<button
+									className="btn btn-outline-secondary"
+									type="button"
+									onClick={add_Task}>
+									Add Task
+								</button>
+							</div>
 						</div>
-					</div>
-				</li>
+					</li>
+					{taskLists !== [] ? (
+						<div>
+							{taskLists.map(t => (
+								<li key={t.value} className="list-group-item ">
+									{t.value + " - "}
+									<button
+										className="btn btn-outline-secondary float-right-center"
+										type="button"
+										onClick={e => delete_Task(e, t.id)}>
+										<i className="fas fa-times"></i>
+									</button>
+								</li>
+							))}
+						</div>
+					) : null}
 
-				<li className="list-group-item">
-					<div class="row">
-						<div className="col-10">
-							<h1>Tareas</h1>
-						</div>
-						<div className="col-2">
-							<button
-								className="btn btn-outline-secondary float-right"
-								type="button">
-								<i className="fas fa-times"></i>
-							</button>
-						</div>
-					</div>
-				</li>
-
-				<li className="list-group-item"></li>
-				<li className="list-group-item list-group-item-action list-group-item-dark">
-					Task number
-				</li>
-			</ul>
+					<li className="list-group-item"></li>
+					<li className="list-group-item list-group-item-action list-group-item-dark">
+						Task number {count()}
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
